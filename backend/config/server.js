2a61392d075a95ab084a9a8e54a662bbf6e3dd9e
koshiro-adachi <koushiro54600404@gmail.com>
2025-06-07 21:31:24 +0900

    nodejsのバージョン指定とbackend環境（strapi）のインストール

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '72947d7e38d9051f490b3fac54736b04'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f5825968-8522-47dd-8289-1d62496fee0d'
+};
