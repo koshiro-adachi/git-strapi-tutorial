4485fa731a3e31d5c129886b714fb638fed8155c
koshiro-adachi <koushiro54600404@gmail.com>
2025-06-07 16:14:57 +0900

    不用なHeadを削除

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import App from "next/app";
-import Head from "next/head";
 import Layout from "../components/layout";
 
 export default class MyApp extends App {
@@ -8,12 +7,6 @@ export default class MyApp extends App {
         const { Component, pageProps } = this.props;
         return (
             <>
-                <Head>
-                    <link
-                        rel="stylesheet"
-                        href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-                    />
-                </Head>
                 <Layout>
                     <Component {...pageProps} />
                 </Layout>
